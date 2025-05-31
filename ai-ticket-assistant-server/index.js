72378e0a480e6e161b77a0b2de5e89c3241b5c09
Shekhar Thakur <122804156+shekharrs@users.noreply.github.com>
2025-05-31 14:35:45 +0000

    done with basic setup

---
+++ ai-ticket-assistant-server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected ✅");
+        app.listen(PORT, () => console.log('🚀 app listening on port ${PORT}'))
+    })
+    .catch((err) => console.error("❌ MongoDB error: ", err))
